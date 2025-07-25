3f449a1f306677d66fa5382ed30a7c788d27570a
ragab45 <115900223+ragab45@users.noreply.github.com>
2025-07-25 18:11:28 +0300

    V-1

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -2,9 +2,40 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+       
+            #region V-1
+             public struct PhoneBook
         {
-            Console.WriteLine("Hello, World!");
+            public long this[string name]
+            {
+                get
+                {
+                    for (int i = 0; i < names.Length; i++)
+                    {
+                        if (names[i] == name)
+                            return numbers[i];
+                    }
+                    return -1;
+                }
+                set
+                {
+                    for (int i = 0; i < names.Length; i++)
+                    {
+                        if (names[i] == name)
+                            numbers[i] = value;
+                    }
+                }
+            }
+
+            public string this[int index]
+            {
+                get
+                {
+                    return $"Index = {index}, Name = {names[index]}, Number = {numbers[index]}";
+                }
+            }
         }
+        #endregion
     }
+    
 }
